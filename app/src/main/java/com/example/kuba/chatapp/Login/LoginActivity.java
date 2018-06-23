b265aa82b32ee3101d69fe027723e0d1b4d6650b
jakubbialass <35003151+jakubbialass@users.noreply.github.com>
2018-06-23 11:50:03 +0200

    first

---
+++ app/src/main/java/com/example/kuba/chatapp/Login/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.kuba.chatapp.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kuba.chatapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+
+    }
+
+
+
+
+
+
+}
